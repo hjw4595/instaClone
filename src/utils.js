30f1d239b873f67d7363c4168a530c192e785855
hjw4595 <hongjung73@gmail.com>
2019-07-16 23:25:46 +0900

    create request words(fix)

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
-import { adjectives , nouns } from "./words";
+import { adjectives, nouns } from "./words";
 
-export const generatorSecret = () => {
-    const randomNumber = math.floor(Math.random() * adjectives.length);
-    return '${adjectives[randomNumer]} ${nouns[randomNumber]}'
-}
+export const generateSecret = () => {
+  const randomNumber = Math.floor(Math.random() * adjectives.length);
+  return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
